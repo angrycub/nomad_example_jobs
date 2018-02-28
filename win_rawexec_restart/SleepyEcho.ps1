7e240787d3082243adaf69b490a2f1d144b3a349
Charlie Voiselle <charlie.voiselle@gmail.com>
2018-02-28 13:27:04 -0800

    Update SleepyEcho.ps1
    Added Vault Token to SleepyEcho

--- win_rawexec_restart/SleepyEcho.ps1
+++ win_rawexec_restart/SleepyEcho.ps1
@@ -4,6 +4,7 @@ if (-not (Test-Path env:SLEEP_SECS)) { $env:SLEEP_SECS = 2 }
 
 while ($true) { 
   if (Test-Path env:EXTRAS) { $extras=" EXTRAS: $env:EXTRAS" } else {$extras=""}
-  "$(get-date) -- Alive... going back to sleep for $env:SLEEP_SECS seconds. $extras"
+  if (Test-Path env:VAULT_TOKEN) { $vt=" VAULT_TOKEN: $env:VAULT_TOKEN" } else {$vt=""}
+  "$(get-date) -- Alive... going back to sleep for $env:SLEEP_SECS seconds. $vt $extras"
   start-sleep $env:SLEEP_SECS
 } 
