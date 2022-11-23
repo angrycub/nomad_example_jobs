43892fe895c47a0166917c7b85249dc2a8a30e9a
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-11-23 15:32:22 -0500

    Updating testing task

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/perl -w
 use strict;
 use Data::Dumper;
+use File::Basename;
 
 my ($jobName) = @ARGV;
 
 if (not defined $jobName) {
   die "Need a path to a Nomad job file.\n";
 }
+
+my $dirname  = dirname($jobName);
+chdir "$dirname";
 
 my $prog = "nomad";
 my @args = ("job", "plan");
@@ -30,19 +34,32 @@ if ($exitCode == 255) {
   print "I'm an exitcode 255!\n";
   # try parsing the error some?
   if ($return_value =~ "Failed to parse using HCL 2" ) {
-    print "I'm here!\n";
     my $re = '/.*:\n([^:]+):(\d+),(\d+)-(\d+)\n(.*)/m';
     my @matches = $return_value =~ $re;
     # Print the entire match result
-    print Dumper(@matches);
-    exit
-
+    if (@matches) {
+      print Dumper(@matches);
+    } else {
+      print "***Failed to parse ***\n";
+      print $return_value;
+    }
+  } else {
+    print "*** Not an HCL2 error ***\n";
+    print $return_value;
   }
+  exit $exitCode;
 }
+if ($exitCode == 1) {
+   print "Maybe Successful...Review output\n";
+   print $return_value;
+   exit;
+}
+
 if ($exitCode == 0) {
     print "Success!\n";
 }
 else {
-    printf "Failed with error code %d\nError message: %s\n",
-        $exitCode>>8, $return_value;
+    printf "Failed with error code %d\n\nError message: %s\n",
+        $exitCode, $return_value;
 }
+exit $exitCode
